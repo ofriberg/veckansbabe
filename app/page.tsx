bf31b044f053c29dd23ac435aeab811721a4dfe3
Oscar Friberg <oscfriberg@gmail.com>
2024-08-31 09:41:54 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
       <Image
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Hej14aug2010.JPG/800px-Hej14aug2010.JPG"
         alt="hej Logo"
         priority
         width={200}
         height={200}
+        layout="responsive"
       />
     </main>
   );
